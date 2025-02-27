ce24338fa44686426ebcb081eebd8f178f1b4db3
QCAT FERMI <qcatfermi@gmail.com>
2025-02-27 19:07:05 +0800

    Delete 1.0.1 directory

--- 1.0.1/proto/quant.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Particles:
-    def __init__(self, num_particles, domain_size):
-        self.num_particles = num_particles
-        self.positions = np.random.rand(num_particles) * domain_size
-        self.velocities = np.zeros(num_particles)
-        self.charges = np.ones(num_particles)
-        self.masses = np.ones(num_particles)
--- 1.0.1/proto/testcode.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-e = 1.5
-x = np.full(10, e)
-
-print(x)
